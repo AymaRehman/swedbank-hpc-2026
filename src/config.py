ce201cb547288fa62b0b336d51fac21324042936
AymaRehman <ayma.rehman24@gmail.com>
2026-03-20 22:56:41 +0200

    Added model dir in config.py

--- src/config.py
+++ src/config.py
@@ -4,3 +4,4 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATA_RAW_PATH = BASE_DIR / "data" / "spam.csv"
 DATA_PROCESSED_PATH = BASE_DIR / "data" / "processed" / "sms_clean.csv"
+MODELS_DIR = BASE_DIR / "models"
