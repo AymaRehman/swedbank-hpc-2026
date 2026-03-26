4d998c54d356d74786aaecd3d2edf50d4c889a0c
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2026-03-27 00:41:04 +0200

    edited job.sh to make use of vasara partition of the cluster that has 32 newer cores

--- hpc/grid_search.sh
+++ hpc/grid_search.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #PBS -N grid_search
 #PBS -l walltime=02:00:00
-#PBS -l procs=16
+#PBS -l procs=32
+#PBS -l feature=vasara
 #PBS -j oe
 #PBS -o /mnt/home/hpc00523/swedbank-hpc-2026/hpc/results/grid_search_output.txt
 #PBS -A hpc_mt_00f65_hpcallocforgroup
@@ -13,11 +14,11 @@ source venv/bin/activate
 
 echo "Job started: $(date)"
 echo "Running on: $(hostname)"
-echo "CPUs requested: 16"
+echo "CPUs requested: 32"
 
-export OMP_NUM_THREADS=16
-export MKL_NUM_THREADS=16
-export NUMEXPR_NUM_THREADS=16
+export OMP_NUM_THREADS=32
+export MKL_NUM_THREADS=32
+export NUMEXPR_NUM_THREADS=32
 python src/grid_search.py
 
 echo "Job finished: $(date)"
